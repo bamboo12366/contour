00c65797e8e282607aaad8b8b4d9af7e9a77966b
Xinzhu Zhou <xinzhu.zhou@shopee.com>
2021-01-19 17:15:00 +0800

    update

--- apis/projectcontour/v1/httpproxy.go
+++ apis/projectcontour/v1/httpproxy.go
@@ -545,7 +545,7 @@ type LoadBalancerPolicy struct {
 	// and `Cookie`. If an unknown strategy name is specified
 	// or no policy is supplied, the default `RoundRobin` policy
 	// is used.
-	// +kubebuilder:validation:Enum=roundrobin;static-rr;leastconn;firsat;source;uri;url_param;hdr;random;rdp-cookie
+	// +kubebuilder:validation:Enum=roundrobin;static-rr;leastconn;first;source;uri;url_param;random;rdp-cookie
 	Strategy string `json:"strategy,omitempty"`
 }
 
